28d3f4142c204a7dd5182ca311ea85ccaad3eb71
gabrielauzier <gabrielvascoauzier@gmail.com>
2023-11-28 22:26:59 -0400

    chore: config styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,10 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-inter',
+})
 
 export const metadata: Metadata = {
   title: 'devstore',
@@ -14,8 +17,8 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="pt" className={inter.variable}>
+      <body>{children}</body>
     </html>
   )
 }
